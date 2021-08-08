85847ee2feeefe9d3810becf1111f4f310a2b02a
talha aamir <talhaaamir@ricult.com>
2021-08-08 20:01:02 +0500

    Adding ssl to dev db init options | Fixing issue with using db once server is deployed

--- config/envConfig.js
+++ config/envConfig.js
@@ -3,7 +3,10 @@ const localConfig = {
 }
 
 const devConfig = {
-    dbUrl: "postgres://osiottkgizhbqw:8c6a71c79f689aced94bb3cede7d9547e39665735dcbcb0153d6f63900805ea8@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d8ns98jtfvod41",
+    dbUrl: {
+        connectionString: "postgres://osiottkgizhbqw:8c6a71c79f689aced94bb3cede7d9547e39665735dcbcb0153d6f63900805ea8@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d8ns98jtfvod41",
+        ssl: true,
+    }
 }
 
 const whitelistOrigins = ['https://readsyncpdf.web.app','http://localhost:3000'];
