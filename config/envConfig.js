4cac242d389d91f9174b8ba12431673d6c1dfc7c
talha aamir <talhaaamir@ricult.com>
2021-08-08 20:06:37 +0500

    Trying ssl config fix | postgresql ssl validation error for db

--- config/envConfig.js
+++ config/envConfig.js
@@ -5,7 +5,7 @@ const localConfig = {
 const devConfig = {
     dbUrl: {
         connectionString: "postgres://osiottkgizhbqw:8c6a71c79f689aced94bb3cede7d9547e39665735dcbcb0153d6f63900805ea8@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d8ns98jtfvod41",
-        ssl: true,
+        ssl: {rejectUnauthorized: false},
     }
 }
 
